c7055f42a602098062f7b92a1c7c6ded767e73dd
Andrea Bender <andreab.1726@gmail.com>
2019-11-01 14:15:34 -0600

    incorporated moviedex files into new repo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 /* eslint-disable strict */
 require('dotenv').config();
 const express = require('express');
+const movieData = require('./movieData.json');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -16,6 +17,42 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+//Authentication 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  
+  next();
+});
+
+app.get('/movie', (req, res) => {
+  let movies = movieData;
+  const { genre, country, avg_vote } = req.query;
+
+  if(genre){
+    movies = movies.filter(movie => 
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+
+  if(country){
+    movies = movies.filter(place =>
+      place.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+  
+  if(avg_vote){
+    movies = movies.filter(vote => 
+      vote.avg_vote >= avg_vote);
+  }
+
+  res.json(movies);
+});
 
 app.get('/', (req, res) =>{
   res.json({message: 'Hello, world!'});
